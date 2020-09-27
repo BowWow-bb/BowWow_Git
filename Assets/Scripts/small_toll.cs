219d0c363f79cb80e9be63f2142ab70ca6a62e9a
daeun <sung980524@naver.com>
2020-09-27 15:53:01 +0900

    스몰 톨이 움직임 구현
    랜덤으로 움직이게 구현

---
+++ Assets/Scripts/small_toll.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class small_toll : MonoBehaviour
+{
+    public float movePower = 1f;
+
+    Vector3 movement;
+    int movementFlag = 0;//0: 정지, 1: 왼쪽, 2: 오른쪽 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ChangeMovement");
+    }
+
+    IEnumerator ChangeMovement()
+    {
+        movementFlag = Random.Range(0, 3);//움직임 설정
+            
+        yield return new WaitForSeconds(3f);//3초 기다리기 
+
+        StartCoroutine("ChangeMovement");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if(movementFlag ==1)
+        {
+            moveVelocity = Vector3.left;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(movementFlag ==2)
+        {
+            moveVelocity = Vector3.right;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+}

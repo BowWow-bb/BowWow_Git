e56b371735dd8e032ab0a456f0a83884fcfc6695
haneulhaneul <tmajvm7960@naver.com>
2020-09-29 01:32:21 +0900

    서먼테크 기초까지..
    오늘도 김밥 내일도 김밥

--- Assets/Bigtol/BigtolAi.cs
+++ Assets/Bigtol/BigtolAi.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BigtolAi : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject bigfire;
+
     float hp;               //HP
     static float hp_std;    //HP 손상 기준
 
@@ -13,10 +16,13 @@ public class BigtolAi : MonoBehaviour
     Vector3 dir;            //이동 벡터
     int dir_ran;            //랜덤 이동방향 0:왼쪽, 1:오른쪽
 
+    int cnt;
+
     // Start is called before the first frame update
     void Start()
     {
         Init(); //초기 설정값 
+        cnt = 0;
     }
 
     // Update is called once per frame
@@ -30,7 +36,7 @@ public class BigtolAi : MonoBehaviour
 
             if (dir_ran == 0)   //왼쪽 이동
             {
-                if (transform.position.x - dis_std * Time.deltaTime < -50.0f)
+                if (transform.position.x - dis_std * Time.deltaTime < -43.0f)
                     dis_tmp= 0.0f;
                 else
                 {
@@ -40,7 +46,7 @@ public class BigtolAi : MonoBehaviour
             }
             else //오른쪽 이동
             {
-                if (transform.position.x + dis_std * Time.deltaTime > 50.0f)
+                if (transform.position.x + dis_std * Time.deltaTime > 43.0f)
                     dis_tmp = 0.0f;
                 else
                 {
@@ -52,13 +58,25 @@ public class BigtolAi : MonoBehaviour
             if (dis_tmp > dis_std) //일정거리 이동 후 초기화
                 dis_tmp = 0.0f;
         }
-    
-        else if (hp < 100.0f) //손상된 경우 - 플레이어 향해 이동
-        {
 
+        //else if (hp < 100.0f) //손상된 경우 - 플레이어 향해 이동
+        //{
+
+        //}
+
+        //if(hp<빅파이어볼 생성 기준) -> 빅파이어볼 생성 (조건문 나중에 만들자..) **무한 생성 방지 방법 생각
+        if (cnt == 0) //확인위해 임시 조건문.. 나중에 삭제
+        {
+            GameObject ball = GameObject.Instantiate(bigfire); //파이어볼 생성
+
+            //빅파이어볼 초기 위치 = 빅톨 현재 위치 (파이어볼의 크기 고려해 위로 이동)
+            ball.transform.position = new Vector3(transform.position.x, transform.position.y + 5.0f, transform.position.z);
+
+            cnt++;
         }
-    
-    
+       
+
+
     }
 
     void Init()
---
+++ Assets/Bigtol/FireSballMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSballMove : MonoBehaviour
+{
+    //float tmp_x;    //이동 랜덤 난수
+    //float tmp_y;    //이동 랜덤 난수
+    float v;        //이동 속도
+    //Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pos = transform.position;
+        v = 12.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //tmp_x= Random.Range(-10.0f,10.0f);   //-10.0 ~ 10.0 랜덤 난수 발생
+        //tmp_y= Random.Range(-10.0f, 10.0f);   //-10.0 ~ 10.0 랜덤 난수 발생
+        //Vector3 dir = new Vector3(transform.position.x+tmp_x,transform.position.y+tmp_y,transform.position.z);
+        //transform.position = (transform.forward * Time.deltaTime * v) + pos;
+        transform.position += transform.forward * Time.deltaTime * v;
+    }
+}

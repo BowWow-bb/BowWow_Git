55c4aad2cc2afdaa07ff04ed3cb3cd165af61614
daeun <sung980524@naver.com>
2020-09-28 12:01:25 +0900

    스몰톨, 파이어볼

--- Assets/Smalltol/Scripts/small_toll.cs
+++ Assets/Smalltol/Scripts/small_toll.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class small_toll : MonoBehaviour
 {
+    //움직임 
     public float movePower = 1f;
-
     Vector3 movement;
     int movementFlag = 0;//0: 정지, 1: 왼쪽, 2: 오른쪽 
+
+    //파이어볼 
+    public GameObject fireballPrefab;//파이어볼 프리팹
+    public float RateMin = 0.5f;//일단 발사 주기 임의로 설정 
+    public float RateMax = 3f;
+
+    private Transform target;//플레이어 위치 
+    private float Rate;//생성 주기
+    private float timeAfter;//최근 생성 시점에서 지난 시간
 
     // Start is called before the first frame update
     void Start()
     {
+        timeAfter = 0f;
+        //traget = 플레이어위치;
+        Rate = Random.Range(RateMin, RateMax);
+
         StartCoroutine("ChangeMovement");
     }
 
@@ -45,5 +58,9 @@ public class small_toll : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
         transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+    void FireballMake()
+    {
+        
     }
 }

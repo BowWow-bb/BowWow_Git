36d1726b20a738c8ccec32c8b82b3554b26a517c
daeun <sung980524@naver.com>
2020-09-29 17:08:21 +0900

    파이어볼 아직 하다말음
    일정 거리 내에 들어오면 스몰톨이 따라옴

--- Assets/Smalltol/Scripts/small_toll.cs
+++ Assets/Smalltol/Scripts/small_toll.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class small_toll : MonoBehaviour
 {
-    //움직임 
-    public float movePower = 1f;
-    Vector3 movement;
-    int movementFlag = 0;//0: 정지, 1: 왼쪽, 2: 오른쪽 
-
-    //파이어볼
-    bool fireballmake=false;
     public GameObject fireballPrefab;//파이어볼 프리팹
-   
-    public float RateMin = 0.5f;//일단 발사 주기 임의로 설정 
-    public float RateMax = 3f;
+    public GameObject DDaeng;//땡이
 
-    private Transform target;//플레이어 위치 
-    private float Rate;//생성 주기
-    private float timeAfter;//최근 생성 시점에서 지난 시간
+    Vector3 target;//땡이 위치
+
+    public float d = 20f;//범위 거리 설정  
+    public float movePower = 1f;//움직이는 속력
+    public float RateMin = 0.5f;//최소 생성 주기 
+    public float RateMax = 3f;//최대 생성 주기
+    public float Rate;
+
+    private float timeAfter;//발사 후 지난 시간 
+
+    int movementFlag = 0;//0: 정지, 1: 왼쪽, 2: 오른쪽
+    
+    bool isTracing = false;//거리 내에 들어와서 유지 중인 상태 
+    bool Enter = false;//거리 내에 들어오면 (처음)
+
 
     // Start is called before the first frame update
     void Start()
     {
         timeAfter = 0f;
-        //traget = 플레이어위치;
         Rate = Random.Range(RateMin, RateMax);
 
         StartCoroutine("ChangeMovement");
@@ -42,34 +44,84 @@ public class small_toll : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        timeAfter += Time.deltaTime;//시간 갱신
+     
+        Distance();
         Move();
+    }
+
+    void Distance()//거리 파악 
+    {
+        target = DDaeng.transform.position;
+        float distance = Vector3.Distance(target, transform.position);
+
+        //Debug.Log("땡이랑 거리: "+distance);
+
+        if (distance<= d)//범위 내에 처음 들어오면
+        {
+            Enter = true;
+            Debug.Log("범위 내에 들어옴");
+            StopCoroutine("ChangeMovement");
+        }
+
+        if (Enter == true && distance <= d)//범위 내에 계속 있으면 
+        {
+            isTracing = true;
+        }
+        
+        if(isTracing == true && distance > d)//거리 벗어나면 
+        {
+            Enter = false;
+            isTracing = false;
+            StartCoroutine("ChangeMovement");
+        }
     }
 
     void Move()
     {
         Vector3 moveVelocity = Vector3.zero;
+        string dist = "";
 
-        if(movementFlag ==1)
+        if(isTracing)//일정 거리 내이면 추적 
+        {
+            if (target.x < transform.position.x)//땡이가 왼쪽이면 
+                dist = "Left";
+            else if (target.x > transform.position.x)//땡이가 오른쪽이면 
+                dist = "Right";
+        }
+        else//거리 밖이면 
+        {
+            if (movementFlag == 1)
+                dist = "Left";
+            else if (movementFlag == 2)
+                dist = "Right";
+            else//멈춘 상태이면 
+            {
+                if (timeAfter >= Rate)
+                {
+                    timeAfter = 0f;
+                    FireballMake();
+                }
+            }
+        }
+
+        if(dist =="Left")
         {
             moveVelocity = Vector3.left;
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if(movementFlag ==2)
+        else if(dist == "Right")
         {
             moveVelocity = Vector3.right;
             transform.localScale = new Vector3(-1, 1, 1);
-            if(fireballmake == false)
-            {
-                FireballMake();//생성
-            }
         }
         transform.position += moveVelocity * movePower * Time.deltaTime;
     }
     void FireballMake()
     {
         GameObject ball = GameObject.Instantiate(fireballPrefab); //파이어볼 생성
-        ball.transform.position = new Vector3(transform.position.x, transform.position.y+0.5f, transform.position.z+20.0f);
+        ball.transform.position = new Vector3(transform.position.x, transform.position.y+0.5f, transform.position.z+20.0f);//파이어볼 초기 위치 
 
-        fireballmake = true;
+        Rate = Random.Range(RateMin, RateMax);//다음 번 파이어볼 생성 주기 설정 
     }
 }
---
+++ Assets/Smalltol/small_fireball.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class small_fireball : MonoBehaviour
+{
+    //파이어볼 움직임
+    public GameObject smalltall;//스몰톨 가져오기
+    public GameObject DDaeng;//플레이어 위치
+
+    Vector3 ball;
+    Vector3 me;
+    Vector3 target;
+
+    public float movePower = 1f;
+    float gravity = 9.8f;
+    float accel = 0f;//가속도
+    float c = 0.7f;//탄성계수 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        smalltall = GameObject.Find("Smalltol");
+        DDaeng = GameObject.Find("DDaeng");
+
+        Destroy(gameObject, 5.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ball = transform.position;//파이어볼의 위치
+        me = smalltall.transform.position;//스몰톨의 위치 
+        target = DDaeng.transform.position;
+
+     
+        //Debug.Log("player위치: " + target.x);//파이어볼이 생성될 때 플레이어의 위치
+
+        Vector3 moveVelocity = Vector3.zero;
+
+        //땡이가 왼쪽
+        if (target.x < me.x)
+        {
+            moveVelocity = Vector3.left;
+        }
+        //땡이가 오른쪽
+        if (target.x > me.x)
+        {
+            moveVelocity = Vector3.right;
+        }
+        transform.position += moveVelocity * movePower * Time.deltaTime*ball.y;
+    }
+}

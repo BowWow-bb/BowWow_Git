3e1c632ae90ae3ac333e0ab1b44b30abc431cf27
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2020-10-19 19:53:15 +0900

    ㄴㅇ
    ㄴㅇ

--- Assets/Map/Scripts/itemBone.cs
+++ Assets/Map/Scripts/itemBone.cs
@@ -8,8 +8,6 @@ public class itemBone : MonoBehaviour
     // Start is called before the first frame update
     float t;    //타이머
     // Start is called before the first frame update
-    public GameObject imageObj;
-    public Sprite myimage;
 
     AudioSource Item_pick;
     AudioSource Item_drop;
@@ -17,8 +15,11 @@ public class itemBone : MonoBehaviour
     public AudioClip Item_drop_Sound;
     public AudioClip Item_Sound;
 
+    GameObject Q;
+
     void Start()
     {
+        Q = GameObject.Find("Q");
         t = 0;
         Item_pick = gameObject.AddComponent<AudioSource>();
         Item_pick.clip = Item_Sound;
@@ -46,19 +47,13 @@ public class itemBone : MonoBehaviour
         if (other.gameObject.GetComponent<Move>() != null)  //땡이와 충돌한 경우
         {
             Item_pick.Play();
+            Q.SetActive(false);
             if (Input.GetKeyDown(KeyCode.Z))
             {
                 Debug.Log("땡이가 먹음");
                 Destroy(gameObject);
             }
-            //  if (Input.GetKeyDown(KeyCode.Z))
-            {
-                //Debug.Log("땡이가 먹음");
-                //imageObj = GameObject.FindWithTag("Q");
-                //imageObj.GetComponent<SpriteRenderer>().sprite = myimage;
-
-                //Destroy(gameObject);
-            }
+ 
             //Move DD = GameObject.Find("DDaeng").GetComponent<Move>();
             //if(DD.eatFlag)    //플레이어가 아이템을 습득한 경우
             //{
